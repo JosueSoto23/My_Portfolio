26faf21ba6557b7cc4d98c7f76518294b0087fa5
Josue Soto <deivysoto66@gmail.com>
2023-05-03 19:23:35 -0600

    Tailwind configuration fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,18 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import Header from "../../components/Header";
+import Head from "next/head";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to my portfolio! I'm {} 
-        <a
-          className="text-blue-600 hover:underline"
-          href="https://www.linkedin.com/in/josué-soto-2364651b9/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Josué Soto
-        </a>
-      </h1>
-    </main>
-  )
+    <div>
+      <Head>
+        <title> Josue's Portfolio </title>
+      </Head>
+
+      <Header />
+    </div>
+  );
 }
